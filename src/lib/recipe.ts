d8ffd7392bd77c187e91e6fb6e30134196a4cc41
profikid <profikid@gmail.com>
2024-08-09 14:21:48 +0200

    fix

---
+++ src/lib/recipe.ts
@@ -0,0 +1,136 @@
+
+export const initData = {
+    'Dried chilies': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.5, price: 6.75 }] }
+      ],
+      v: 500,
+      r: 400
+    },
+    'Sichuan pepper': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.113, price: 4.99 }] }
+      ],
+      v: 113,
+      r: 60
+    },
+    'Shaoxing wine': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.5, price: 3.39 }] }
+      ],
+      v: 500,
+      r: 240
+    },
+    'Beef tallow': {
+      suppliers: [
+        { name: 'Jumbo', prices: [{ kg: 1, price: 5.79 }] }
+      ],
+      v: 1000,
+      r: 800
+    },
+    'Rapeseed oil': {
+      suppliers: [
+        { name: 'Amazing oriental', prices: [{ kg: 0.210, price: 3.99 }] }
+      ],
+      v: 210,
+      r: 200
+    },
+    'Star anise': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 454,
+      r: 8
+    },
+    'Cassia cinnamon': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 454,
+      r: 16
+    },
+    'Bay leaves': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 454,
+      r: 4.8
+    },
+    'Chinese black cardamom': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 1,
+      r: 12
+    },
+    'Green cardamom': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.005, price: 4.29 }] }
+      ],
+      v: 5,
+      r: 4
+    },
+    'Fennel seeds': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 454,
+      r: 20
+    },
+    'Onion': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 1, price: 2.25 }] }
+      ],
+      v: 1000,
+      r: 300
+    },
+    'Scallions': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.110, price: 1.29 }] }
+      ],
+      v: 110,
+      r: 300
+    },
+    'Cilantro': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.040, price: 2.75 }] }
+      ],
+      v: 30,
+      r: 40
+    },
+    'Ginger': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.351, price: 2.09 }] }
+      ],
+      v: 351,
+      r: 120
+    },
+    'Sichuan chilli bean paste': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.5, price: 3.39 }] }
+      ],
+      v: 500,
+      r: 200
+    },
+    'Fermented black beans': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 1, price: 15 }, { kg: 3, price: 15 }] }
+      ],
+      v: 200,
+      r: 64
+    },
+    'Rock sugar': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.5, price: 2.3 }, { kg: 1, price: 5 }] }
+      ],
+      v: 200,
+      r: 48
+    },
+    'Orange peel': {
+      suppliers: [
+        { name: 'Amazing Oriental', prices: [{ kg: 0.454, price: 7.99 }] }
+      ],
+      v: 100,
+      r: 20
+    },
+  };
